7b2083ad36e75f4ccd5603de96591d6c964c9709
oosayeroo <98313914+oosayeroo@users.noreply.github.com>
2022-05-04 08:31:39 +0100

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,84 @@
+Config = {}
+
+Config.GaragePedLocations = {
+    {coords = vector4(136.39, -1278.38, 29.36, 305.41)},
+}
+
+Config.Items = {
+label = "Shop",
+    slots = 9,
+    items = {
+        [1] = {
+            name = "v-shots-pack",
+            price = 25,
+            amount = 50,
+            info = {},
+            type = "item",
+            slot = 1,
+        },
+        [2] = {
+            name = "vodka-crate",
+            price = 130,
+            amount = 50,
+            info = {},
+            type = "item",
+            slot = 2,
+        },
+        [3] = {
+            name = "whiskey-box",
+            price = 115,
+            amount = 50,
+            info = {},
+            type = "item",
+            slot = 3,
+        },
+        [4] = {
+            name = "beer-crate",
+            price = 45,
+            amount = 50,
+            info = {},
+            type = "item",
+            slot = 4,
+        },
+        [5] = {
+            name = "fruit-box",
+            price = 20,
+            amount = 20,
+            info = {},
+            type = "item",
+            slot = 5,
+        },
+        [6] = {
+            name = "tequila-crate",
+            price = 5,
+            amount = 50,
+            info = {},
+            type = "item",
+            slot = 6,
+        },
+        [7] = {
+            name = "v-sugar-syrup",
+            price = 5,
+            amount = 50,
+            info = {},
+            type = "item",
+            slot = 7,
+        },
+        [8] = {
+            name = "coffee",
+            price = 4,
+            amount = 50,
+            info = {},
+            type = "item",
+            slot = 8,
+        },
+        [9] = {
+            name = "v-bitter",
+            price = 4,
+            amount = 50,
+            info = {},
+            type = "item",
+            slot = 9,
+        },
+    }
+}
