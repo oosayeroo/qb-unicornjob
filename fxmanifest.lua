341340205c09cb0be40b0114ff720d3459885c09
oosayeroo <98313914+oosayeroo@users.noreply.github.com>
2022-05-04 02:27:49 +0100

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'oosayeroo'
+description 'qb-unicornjob by oosayeroo'
+version '1.0.0'
+
+shared_scripts {
+    'config.lua'
+}
+
+client_scripts {
+	'client/main.lua',
+	'client/garage.lua',
+	'client/menus.lua'
+}
+
+server_scripts {
+	'server/*.lua'
+}
